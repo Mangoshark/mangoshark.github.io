a442a1c2fe1aedaec8453b7a6c1a5f71c7eb8757
Mangoshark <107973472+Mangoshark@users.noreply.github.com>
2022-06-21 20:58:59 -0700

    website

---
+++ myScript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("testId").innerHTML = "Paragraph changed.";
+
+    window.alert("I see you~~!")
+    }
+
+    var myVariable = "Hello"
+    console.log("myVariable =", myVariable);
